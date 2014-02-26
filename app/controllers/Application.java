e78767860aa8fdf69bb2c4b871b6e16589d0fdd1
Peter Vlugter <pvlugter@gmail.com>
2014-02-26 16:04:44 +1300

    Use java 8 lambdas for websocket callbacks

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -26,26 +26,20 @@ public class Application extends Controller {
             public void onReady(final WebSocket.In<JsonNode> in, final WebSocket.Out<JsonNode> out) {
                 // create a new UserActor and give it the default stocks to watch
                 final ActorRef userActor = Akka.system().actorOf(Props.create(UserActor.class, out));
-                
+
                 // send all WebSocket message to the UserActor
-                in.onMessage(new F.Callback<JsonNode>() {
-                    @Override
-                    public void invoke(JsonNode jsonNode) throws Throwable {
-                        // parse the JSON into WatchStock
-                        WatchStock watchStock = new WatchStock(jsonNode.get("symbol").textValue());
-                        // send the watchStock message to the StocksActor
-                        StocksActor.stocksActor().tell(watchStock, userActor);
-                    }
+                in.onMessage(jsonNode -> {
+                    // parse the JSON into WatchStock
+                    WatchStock watchStock = new WatchStock(jsonNode.get("symbol").textValue());
+                    // send the watchStock message to the StocksActor
+                    StocksActor.stocksActor().tell(watchStock, userActor);
                 });
 
                 // on close, tell the userActor to shutdown
-                in.onClose(new F.Callback0() {
-                    @Override
-                    public void invoke() throws Throwable {
-                        final Option<String> none = Option.empty();
-                        StocksActor.stocksActor().tell(new UnwatchStock(none), userActor);
-                        Akka.system().stop(userActor);
-                    }
+                in.onClose(() -> {
+                    final Option<String> none = Option.empty();
+                    StocksActor.stocksActor().tell(new UnwatchStock(none), userActor);
+                    Akka.system().stop(userActor);
                 });
             }
         };
