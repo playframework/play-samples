80cad3c34877d2489cbd26114cbbf6fed5746a5c
Greg Methvin <greg@methvin.net>
2014-11-06 15:43:08 -0800

    Update for most recent master and try to get tests working again

--- app/GreetingApplicationLoader.scala
+++ app/GreetingApplicationLoader.scala
@@ -3,9 +3,10 @@
 import controllers.Assets
 import play.api.ApplicationLoader.Context
 import play.api._
+import play.api.http._
 import play.api.i18n._
-import play.api.inject.SimpleInjector
-import play.core.DefaultWebCommands
+import play.api.inject.{Injector, NewInstanceInjector, SimpleInjector}
+import play.core.{DefaultWebCommands, Router}
 
 /**
  * Application loader that wires up the application dependencies using Macwire
@@ -20,17 +21,23 @@ class GreetingApplicationLoader extends ApplicationLoader {
     // FIXME: FakeApplication uses the runtime injector so we have to hack around it for now
     val context = new Context(env, None, new DefaultWebCommands, conf)
     val components = new BuiltInComponentsFromContext(context) with GreetingComponents
-    Some(new SimpleInjector(components.injector) +
-      new GlobalPlugin(components.application) +
-      components.injector +
-      components.routes)
+    Some(new SimpleInjector(components.injector) + new GlobalPlugin(components.application))
   }
 }
 
 trait GreetingComponents extends BuiltInComponents with GreetingModule with I18nComponents {
   import com.softwaremill.macwire.MacwireMacros._
 
-  override lazy val global = Global
-  lazy val assets = new Assets
-  lazy val routes = wire[Routes]
+  lazy val assets = wire[Assets]
+  lazy val errorHandler: HttpErrorHandler =
+    new DefaultHttpErrorHandler(environment, configuration, sourceMapper, Some(routes))
+  lazy val routes: Router.Routes = wire[Routes]
+
+  lazy val filters: HttpFilters = NoHttpFilters
+
+  // FIXME: should not need to override the injector here
+  // Seems like in tests we're getting instances of GlobalSettings and JavaCompatibleHttpRequestHandler
+  override lazy val injector: Injector = {
+    new SimpleInjector(NewInstanceInjector) + routes + crypto + httpConfiguration + (DefaultGlobal: GlobalSettings)
+  }
 }
