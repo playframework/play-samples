6412c48b8fb751ec27a9c6d28927b095eba7805d
Will Sargent <will.sargent@gmail.com>
2017-03-28 15:38:45 -0700

    Add integration patience

--- LICENSE.md
+++
@@ -1,1 +0,0 @@
-You can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission. 
--- test/ServerSpec.scala
+++ test/ServerSpec.scala
@@ -1,17 +1,17 @@
-import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatestplus.play._
 import play.api.mvc.Results
 import play.api.test.Helpers._
-import play.api.test.{Injecting, WsTestClient}
+import play.api.test.WsTestClient
 
 class ServerSpec extends PlaySpec
   with BaseOneServerPerSuite
   with GreeterApplicationFactory
   with Results
-  with ScalaFutures {
+  with ScalaFutures
+  with IntegrationPatience {
 
   "Server query should" should {
-
     "work" in {
       WsTestClient.withClient { implicit client =>
         whenReady(wsUrl("/").get) { response =>
