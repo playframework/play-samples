c815c64461f369a66fcf3eb00a7c4dffc51979cb
Will Sargent <wsargent@users.noreply.github.com>
2017-04-10 12:35:13 -0700

    Fix a minor issue with config parsing (#19)

--- app/services/session/ClusterSystem.scala
+++ app/services/session/ClusterSystem.scala
@@ -4,6 +4,7 @@ import javax.inject.Inject
 
 import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
+import play.api.Configuration
 import play.api.inject.ApplicationLifecycle
 
 import scala.concurrent.Future
@@ -15,14 +16,14 @@ import scala.concurrent.Future
  * Normally you'd run several play instances, and the port would be the
  * same while you had several different ip addresses.
  */
-class ClusterSystem @Inject()(applicationLifecycle: ApplicationLifecycle) {
+class ClusterSystem @Inject()(configuration: Configuration, applicationLifecycle: ApplicationLifecycle) {
   private val systems = startup(Seq("2551", "2552"))
 
   def startup(ports: Seq[String]): Seq[ActorSystem] = {
     ports.map { port =>
       // Override the configuration of the port
       val config = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port).
-        withFallback(ConfigFactory.load())
+        withFallback(configuration.underlying)
 
       // use the same name as Play's application actor system, because these are
       // supposed to be "remote" play instances all sharing a distribute cache
