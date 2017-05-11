c5b02c82e2b32eec558e8c31cf545c6dc08342f9
Will Sargent <will.sargent@lightbend.com>
2017-05-11 11:40:44 -0700

    Upgrade to 2.6.x

--- build.sbt
+++ build.sbt
@@ -1,11 +1,11 @@
 name := """play-java-forms-example"""
 
-version := "2.5.x"
+version := "2.6.x"
 
 lazy val root = (project in file(".")).enablePlugins(PlayJava)
 
-scalaVersion := "2.11.11"
+scalaVersion := "2.12.2"
 
 testOptions in Test := Seq(Tests.Argument(TestFrameworks.JUnit, "-a", "-v"))
 
-libraryDependencies += filters
+libraryDependencies += guice
--- test/controllers/WidgetControllerTest.java
+++ test/controllers/WidgetControllerTest.java
@@ -3,10 +3,6 @@ package controllers;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 import play.Application;
-import play.filters.csrf.CSRF;
-import play.filters.csrf.CSRFConfig;
-import play.filters.csrf.CSRFConfigProvider;
-import play.filters.csrf.CSRFFilter;
 import play.inject.guice.GuiceApplicationBuilder;
 import play.mvc.Http;
 import play.mvc.Result;
@@ -41,25 +37,13 @@ public class WidgetControllerTest extends WithApplication {
 
     @Test
     public void testCreateWidget() {
-        Http.RequestBuilder request = addCsrfToken(Helpers.fakeRequest()
+        Http.RequestBuilder request = Helpers.fakeRequest()
                 .method(POST)
-                .bodyForm(ImmutableMap.of("name","widget 6", "price", "6")))
+                .bodyForm(ImmutableMap.of("name","widget 6", "price", "6"))
                 .uri("/widgets");
 
         Result result = route(app, request);
         assertEquals(SEE_OTHER, result.status());
-    }
-
-    private Http.RequestBuilder addCsrfToken(Http.RequestBuilder requestBuilder) {
-        final CSRFFilter csrfFilter = app.injector().instanceOf(CSRFFilter.class);
-        final CSRFConfig csrfConfig = app.injector().instanceOf(CSRFConfigProvider.class).get();
-        final String token = csrfFilter.tokenProvider().generateToken();
-
-        requestBuilder.tag(CSRF.Token$.MODULE$.NameRequestTag(), csrfConfig.tokenName());
-        requestBuilder.tag(CSRF.Token$.MODULE$.RequestTag(), token);
-        requestBuilder.header(csrfConfig.headerName(), token);
-
-        return requestBuilder;
     }
 
 }
