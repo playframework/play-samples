8a46028974da2209bf6ddc73f0227a41900715f6
Will Sargent <will.sargent@gmail.com>
2017-05-11 20:55:26 -0700

    Update for distinct DTO

--- app/controllers/WidgetForm.scala
+++ app/controllers/WidgetForm.scala
@@ -1,21 +1,26 @@
 package controllers
 
-import models.Widget
-
 object WidgetForm {
   import play.api.data.Forms._
   import play.api.data.Form
 
   /**
+   * A form processing DTO that maps to the form below.
+   *
+   * Using a class specifically for form binding reduces the chances
+   * of a parameter tampering attack and makes code clearer.
+   */
+  case class Data(name: String, price: Int)
+
+  /**
    * The form definition for the "create a widget" form.
    * It specifies the form fields and their types,
-   * as well as how to convert from a Widget to form data and vice versa.
+   * as well as how to convert from a Data to form data and vice versa.
    */
-  val widgetForm = Form(
+  val form = Form(
     mapping(
-      "name" -> text,
-      "price" -> number
-    )(Widget.apply)(Widget.unapply)
+      "name" -> nonEmptyText,
+      "price" -> number(min = 0)
+    )(Data.apply)(Data.unapply)
   )
-
 }
