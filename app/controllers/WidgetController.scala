00ff48f3a603fa183d265307fe18bc1e0524f847
Will Sargent <will.sargent@gmail.com>
2017-08-27 18:43:23 -0700

    Add tests to forms example

--- app/controllers/WidgetController.scala
+++ app/controllers/WidgetController.scala
@@ -8,12 +8,16 @@ import play.api.i18n._
 import play.api.mvc._
 
 /**
- * The classic WidgetController using I18nSupport.
+ * The classic WidgetController using MessagesAbstractController.
  *
- * I18nSupport provides implicits that create a Messages instances from
+ * Instead of MessagesAbstractController, you can use the I18nSupport trait,
+ * which provides implicits that create a Messages instances from
  * a request using implicit conversion.
+ *
+ * See https://www.playframework.com/documentation/2.6.x/ScalaForms#passing-messagesprovider-to-form-helpers
+ * for details.
  */
-class WidgetController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
+class WidgetController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
   import WidgetForm._
 
   private val widgets = scala.collection.mutable.ArrayBuffer(
@@ -22,19 +26,22 @@ class WidgetController @Inject()(cc: ControllerComponents) extends AbstractContr
     Widget("Widget 3", 789)
   )
 
+  // The URL to the widget.  You can call this directly from the template, but it
+  // can be more convenient to leave the template commpletely stateless i.e. all
+  // of the "WidgetController" references are inside the .scala file.
   private val postUrl = routes.WidgetController.createWidget()
 
   def index = Action {
     Ok(views.html.index())
   }
 
-  def listWidgets = Action { implicit request: Request[AnyContent] =>
+  def listWidgets = Action { implicit request: MessagesRequest[AnyContent] =>
     // Pass an unpopulated form to the template
     Ok(views.html.listWidgets(widgets, form, postUrl))
   }
 
   // This will be the action that handles our form post
-  def createWidget = Action { implicit request: Request[AnyContent] =>
+  def createWidget = Action { implicit request: MessagesRequest[AnyContent] =>
     val errorFunction = { formWithErrors: Form[Data] =>
       // This is the bad case, where the form had validation errors.
       // Let's show the user the form again, with the errors highlighted.
--- build.sbt
+++ build.sbt
@@ -7,3 +7,4 @@ scalaVersion := "2.12.2"
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 libraryDependencies += guice
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.1" % "test"
