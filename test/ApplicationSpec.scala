33cf7531c80c3d720b4627ba46ec0188bf15bfef
Marcos Pereira <marcos.silva@gmail.com>
2017-12-19 17:11:26 -0200

    Add Gradle configuration and Java 9 support (#3)
    * Add Gradle configuration and Java 9 support
    
    * Remove unnecessary code comment

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,4 +1,5 @@
 import org.scalatestplus.play._
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -7,12 +8,12 @@ import play.api.test.Helpers._
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
-class ApplicationSpec extends PlaySpec with OneAppPerTest {
+class ApplicationSpec extends PlaySpec with GuiceOneAppPerTest {
 
   "Routes" should {
 
     "send 404 on a bad request" in  {
-      route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
+      route(app, FakeRequest(GET, "/boum")).map(status) mustBe Some(NOT_FOUND)
     }
 
   }
--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,12 +1,11 @@
 import org.scalatestplus.play._
-import play.api.test._
-import play.api.test.Helpers._
+import org.scalatestplus.play.guice.GuiceOneServerPerTest
 
 /**
  * add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
-class IntegrationSpec extends PlaySpec with OneServerPerTest with OneBrowserPerTest with HtmlUnitFactory {
+class IntegrationSpec extends PlaySpec with GuiceOneServerPerTest with OneBrowserPerTest with HtmlUnitFactory {
 
   "Application" should {
 
