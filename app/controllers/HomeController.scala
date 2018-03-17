dc2b3feaae073211d635899cb958ad7fabc3ca42
Rutaihwa <rutaihwa@users.noreply.github.com>
2018-03-17 19:54:53 +0100

    Changes due to deprecation to Assets finding in 2.6 (#74)
    * Changes due to deprecation to Assets finding in 2.6
    
    * Using implicits to clean HomeController

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import javax.inject._
+
 import play.api.mvc._
 
 /**
@@ -8,7 +9,8 @@ import play.api.mvc._
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents) (implicit assetsFinder: AssetsFinder)
+  extends AbstractController(cc) {
 
   /**
    * Create an Action to render an HTML page with a welcome message.
