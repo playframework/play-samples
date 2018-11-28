8ec93a40fd9cc18c0d9617b1c7f0580eb791eefe
Renato Cavalcanti <renato@cavalcanti.be>
2018-11-28 19:10:20 +0100

    Upgrade branch 2.7.x using TemplateControl (#68)
    * Updated with template-control on 2018-11-16T14:25:16.227Z
      **/plugins.sbt:
        addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.0-RC3")
      **build.gradle:
        def playVersion = "2.7.0-RC3"
      .travis.yml:
        - openjdk10
      .travis.yml:
              jdk: openjdk10
    
    * Complete upgrade to play 2.7

--- app/controllers/JavaTicker.java
+++ app/controllers/JavaTicker.java
@@ -3,30 +3,35 @@
  */
 package controllers;
 
-import akka.NotUsed;
 import akka.actor.Cancellable;
 import akka.stream.javadsl.Source;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.libs.Json;
-import scala.concurrent.duration.Duration;
 
+import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import java.time.temporal.ChronoUnit;
 
 public interface JavaTicker {
 
     default Source<String, ?> getStringSource() {
         final DateTimeFormatter df = DateTimeFormatter.ofPattern("HH mm ss");
-        final Source<String, Cancellable> tickSource = Source.tick(Duration.Zero(), Duration.create(100, MILLISECONDS), "TICK");
+        final Source<String, Cancellable> tickSource =
+            Source.tick(
+                Duration.ZERO,
+                Duration.of(100L, ChronoUnit.MILLIS),
+                "TICK");
         return tickSource.map((tick) -> df.format(ZonedDateTime.now()));
     }
 
     default Source<JsonNode, ?> getJsonSource() {
         final DateTimeFormatter df = DateTimeFormatter.ISO_INSTANT;
-        final Source<String, Cancellable> tickSource = Source.tick(Duration.Zero(), Duration.create(100, MILLISECONDS), "TICK");
+        final Source<String, Cancellable> tickSource = Source.tick(
+            Duration.ZERO,
+            Duration.of(100L, ChronoUnit.MILLIS),
+            "TICK");
         return tickSource.map((tick) -> {
             ObjectNode result = Json.newObject();
             result.put("timestamp", df.format(ZonedDateTime.now()));
--- build.sbt
+++ build.sbt
@@ -2,10 +2,13 @@ name := "play-java-streaming-example"
 
 version := "1.0-SNAPSHOT"
 
-scalaVersion := "2.12.6"
-
-crossScalaVersions := Seq("2.11.12", "2.12.4")
+scalaVersion := "2.12.7"
 
 lazy val root = (project in file(".")).enablePlugins(PlayJava)
 
 libraryDependencies += guice
+
+javacOptions ++= Seq(
+  "-Xlint:unchecked",
+  "-Xlint:deprecation"
+)
