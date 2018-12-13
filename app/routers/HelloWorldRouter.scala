aa584f5f1eeba4182fb0ebf27d0fcf79540c138c
Renato  Cavalcanti <renato@cavalcanti.be>
2018-12-13 10:27:09 +0000

    remove unnessary usage of execution context

--- app/routers/HelloWorldRouter.scala
+++ app/routers/HelloWorldRouter.scala
@@ -7,10 +7,6 @@ import javax.inject.Inject
 import scala.concurrent.Future
 
 class HelloWorldRouter @Inject() (mat: Materializer) extends example.myapp.helloworld.grpc.AbstractGreeterServiceRouter(mat) {
-  private implicit val ec = mat.executionContext
-
   override def sayHello(in: HelloRequest): Future[HelloReply] =
-    Future {
-      HelloReply(s"Hello, ${in.name}!")
-    }
+    Future.successful(HelloReply(s"Hello, ${in.name}!"))
 }
