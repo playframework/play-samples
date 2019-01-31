7aba1b589994fb92404cde015c85c504b24fd641
Ignasi Marimon-Clos <ignasi@lightbend.com>
2019-01-31 17:58:37 +0100

    Cleanup code and comments

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,23 +1,20 @@
 package controllers
 
-import example.myapp.helloworld.grpc.GreeterServiceClient
-import example.myapp.helloworld.grpc.HelloReply
-import example.myapp.helloworld.grpc.HelloRequest
-import javax.inject.Inject
-import akka.stream.Materializer
 import com.typesafe.config.Config
+import example.myapp.helloworld.grpc.{ GreeterServiceClient, HelloReply, HelloRequest }
+import javax.inject.Inject
 import play.api.mvc._
 
-import scala.concurrent.Future
+import scala.concurrent.{ ExecutionContext, Future }
 
-class HomeController @Inject() (greeterServiceClient: GreeterServiceClient, config:Config, mat: Materializer) extends InjectedController {
-  implicit val ec = mat.executionContext
+class HomeController @Inject() (greeterServiceClient: GreeterServiceClient, config:Config)(implicit ec: ExecutionContext) extends InjectedController {
 
   def index = Action.async {
     val request = HelloRequest("Caplin")
+    // create a gRPC request
     val reply: Future[HelloReply] = greeterServiceClient.sayHello(request)
-
-    reply.map(_.message).map(m ⇒ Ok(m))
+    // forward the gRPC response back as a plain String on an HTTP response
+    reply.map(_.message).map(m => Ok(m))
   }
 
 }
