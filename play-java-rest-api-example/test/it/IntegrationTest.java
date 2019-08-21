1f4e28a93e4524ca067f729f18c6d850a193271f
Ignasi Marimon-Clos <ignasi@lightbend.com>
2019-08-21 10:18:38 +0200

    Improves Java REST tests: unique Strings, negative assertions

--- play-java-rest-api-example/test/it/IntegrationTest.java
+++ play-java-rest-api-example/test/it/IntegrationTest.java
@@ -1,6 +1,7 @@
 package it;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.Ignore;
 import org.junit.Test;
 import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
@@ -12,9 +13,15 @@ import v1.post.PostData;
 import v1.post.PostRepository;
 import v1.post.PostResource;
 
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.StreamSupport;
+
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static play.test.Helpers.*;
 
 public class IntegrationTest extends WithApplication {
@@ -27,21 +34,52 @@ public class IntegrationTest extends WithApplication {
     @Test
     public void testList() {
         PostRepository repository = app.injector().instanceOf(PostRepository.class);
-        repository.create(new PostData("title", "body"));
+        repository.create(new PostData("title-of-post-123", "body-123"));
 
         Http.RequestBuilder request = new Http.RequestBuilder()
                 .method(GET)
                 .uri("/v1/posts");
 
         Result result = route(app, request);
-        final String body = contentAsString(result);
-        assertThat(body, containsString("body"));
+
+        assertJsonPayloadHasTitle("title-of-post-123", result) ;
+    }
+
+    @Test
+    public void testListWithTrailingSlash() {
+        PostRepository repository = app.injector().instanceOf(PostRepository.class);
+        repository.create(new PostData("title-of-another-post", "body-456"));
+
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .uri("/v1/posts/");
+
+        Result result = route(app, request);
+        assertJsonPayloadHasTitle("title-of-another-post", result) ;
+    }
+
+    private void assertJsonPayloadHasTitle(String expectedTitle, Result actual){
+        final String responseBody = contentAsString(actual);
+        assertFalse(responseBody.contains("Action Not Found"));
+        JsonNode listOfPosts = Json.parse(responseBody);
+        Iterator<JsonNode> elements = listOfPosts.elements();
+        // spliterator dance to build a Stream from an Iterator
+        Optional<PostResource> post = StreamSupport.stream(
+            Spliterators.spliteratorUnknownSize(
+                elements,
+                Spliterator.ORDERED),
+            false)
+            .map(jsonNode -> Json.fromJson(jsonNode, PostResource.class))
+            .filter(p -> p.getTitle().equals(expectedTitle))
+            .findFirst();
+
+        assertTrue(post.isPresent());
     }
 
     @Test
     public void testTimeoutOnUpdate() {
         PostRepository repository = app.injector().instanceOf(PostRepository.class);
-        repository.create(new PostData("title", "body"));
+        repository.create(new PostData("title-testTimeoutOnUpdate", "body-testTimeoutOnUpdate"));
 
         JsonNode json = Json.toJson(new PostResource("1", "http://localhost:9000/v1/posts/1", "some title", "somebody"));
 
@@ -57,7 +95,7 @@ public class IntegrationTest extends WithApplication {
     @Test
     public void testCircuitBreakerOnShow() {
         PostRepository repository = app.injector().instanceOf(PostRepository.class);
-        repository.create(new PostData("title", "body"));
+        repository.create(new PostData("title-testTimeoutOnUpdate", "body-testCircuitBreakerOnShow"));
 
         Http.RequestBuilder request = new Http.RequestBuilder()
                 .method(GET)
