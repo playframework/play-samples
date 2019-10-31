3edea2bfd126e6f5354575786488d53cab6b7899
Dale Wijnand <dale.wijnand@gmail.com>
2019-10-31 09:22:19 +0000

    Remove need for unsafeUpcast in SessionCache

--- play-scala-secure-session-example/app/services/session/SessionCache.scala
+++ play-scala-secure-session-example/app/services/session/SessionCache.scala
@@ -30,7 +30,7 @@ class SessionCache(
   import SessionExpiration._
   import akka.cluster.ddata.{ LWWMap, LWWMapKey }
   import akka.cluster.ddata.typed.scaladsl.DistributedData
-  import akka.cluster.ddata.typed.scaladsl.Replicator._
+  import akka.cluster.ddata.typed.scaladsl.Replicator.{ Command => _, _ }
   import context.log
 
   private val expirationTime: FiniteDuration = {
@@ -41,44 +41,50 @@ class SessionCache(
   private val distributedData: DistributedData = DistributedData(context.system)
   private[this] implicit val uniqAddress = distributedData.selfUniqueAddress
 
-  def receive: SessionCache.Command => Unit = {
-
+  def behavior(children: Map[String, ActorRef[RefreshSession.type]]): Behavior[Command] = Behaviors.receiveMessage {
     case PutInCache(key, value) =>
-      refreshSessionExpiration(key)
       replicator.askUpdate(Update(dataKey(key), emptyMap, WriteLocal, _)(_ :+ (key -> value)), nop)
+      refreshSessionExpiration(key, children)
 
     case Evict(key) =>
       destroySessionExpiration(key)
       replicator.askUpdate(Update(dataKey(key), emptyMap, WriteLocal, _)(_.remove(uniqAddress, key)), nop)
+      Behaviors.same
 
     case GetFromCache(key, replyTo) =>
       replicator.askGet(Get(dataKey(key), ReadLocal, _), InternalGetResponse(_, replyTo))
+      Behaviors.same
 
     case InternalGetResponse(g @ GetSuccess(mk @ LWWMapKey(key)), replyTo) =>
-      refreshSessionExpiration(key)
-      g.get(mk).get(key) match {
-        case Some(value) => replyTo ! Cached(key, Some(value))
-        case None => replyTo ! Cached(key, None)
-      }
+      replyTo ! Cached(key, g.get(mk).get(key))
+      refreshSessionExpiration(key, children)
 
     case InternalGetResponse(NotFound(LWWMapKey(key)), replyTo) =>
       replyTo ! Cached(key, None)
+      Behaviors.same
 
-    case _: InternalUpdateResponse[_] => // ok
+    case InternalSessionActorTerminated(key) =>
+      behavior(children - key)
+
+    case _: InternalUpdateResponse[_] =>
+      Behaviors.same
   }
 
   private def emptyMap: LWWMap[String, Array[Byte]]                = LWWMap.empty
   private def dataKey(key: String): LWWMapKey[String, Array[Byte]] = LWWMapKey(key)
   private def nop[A](x: A)                                         = InternalUpdateResponse(x)
 
-  private def refreshSessionExpiration(key: String) = {
-    context.child(key) match {
+  private def refreshSessionExpiration(key: String, children: Map[String, ActorRef[RefreshSession.type]]): Behavior[Command] = {
+    children.get(key) match {
       case Some(sessionInstance) =>
         log.info(s"Refreshing session $key")
-        sessionInstance.unsafeUpcast[RefreshSession.type] ! RefreshSession // TODO: make this safe
+        sessionInstance ! RefreshSession
+        Behaviors.same
       case None =>
         log.info(s"Creating new session $key")
-        context.spawn(SessionExpiration(context.self, key, expirationTime), key)
+        val sessionInstance = context.spawn(SessionExpiration(context.self, key, expirationTime), key)
+        context.watchWith(sessionInstance, InternalSessionActorTerminated(key))
+        behavior(children + (key -> sessionInstance))
     }
   }
 
@@ -113,10 +119,11 @@ object SessionCache {
 
   private case class InternalUpdateResponse[A](x: A) extends InternalCommand
 
+  private case class InternalSessionActorTerminated(key: String) extends InternalCommand
+
   def apply() = Behaviors.setup[Command] { context =>
     DistributedData.withReplicatorMessageAdapter[Command, LWWMap[String, Array[Byte]]] { replicator =>
-      val actor = new SessionCache(context, replicator)
-      Behaviors.receiveMessage[Command] { command => actor.receive(command); Behaviors.same }
+      new SessionCache(context, replicator).behavior(Map.empty)
     }
   }
 }
