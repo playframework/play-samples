1328c1c3ad8eff86412f59e83453938becce5809
Ignasi Marimon-Clos <ignasi@lightbend.com>
2020-01-09 12:19:47 +0100

    Moves to conf-based Assets management (like the scala starter sample)

--- play-java-starter-example/app/controllers/HomeController.java
+++ play-java-starter-example/app/controllers/HomeController.java
@@ -4,11 +4,20 @@ import play.mvc.*;
 
 import views.html.*;
 
+import javax.inject.Inject;
+
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+
+    private final AssetsFinder assetsFinder;
+
+    @Inject
+    public HomeController(AssetsFinder assetsFinder) {
+        this.assetsFinder = assetsFinder;
+    }
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -17,7 +26,11 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(
+            index.render(
+                "Your new application is ready.",
+                assetsFinder
+            ));
     }
 
 }
