5b67802973b544f960c58429da12974181038dfb
Ignasi Marimon-Clos <ignasi@lightbend.com>
2020-05-27 17:06:59 +0200

    Adds client and node certificates on the examples

--- play-scala-tls-example/scripts/gencerts.sh
+++ play-scala-tls-example/scripts/gencerts.sh
@@ -8,9 +8,14 @@ echo ${PW} > ${DIR}/password
 ${DIR}/genca.sh
 ${DIR}/genclient.sh
 
+## some server certificates
 ${DIR}/gen-example.com.sh
 ${DIR}/gen-one.example.com.sh
 ${DIR}/gen-two.example.com.sh
 ${DIR}/gen-wildcard.example.com.sh
+## a client certificate
+${DIR}/gen-client.example.com.sh
+## a certificate valid for both server and client (peer-to-peer)
+${DIR}/gen-mode.example.com.sh
 
 ${DIR}/gentrustanchor.sh
