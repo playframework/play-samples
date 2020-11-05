6a683d07384c9ed00fc0c2a45c28b199751ad505
Ignasi Marimon-Clos <ignasi@lightbend.com>
2020-11-05 12:51:28 +0100

    Adds a script to do a quick and dirty run of the samples locally

---
+++ local-test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+pushd play-java-chatroom-example        && scripts/test-sbt && popd
+pushd play-java-compile-di-example      && scripts/test-sbt && popd
+pushd play-java-dagger2-example         && scripts/test-sbt && popd
+pushd play-java-ebean-example           && scripts/test-sbt && popd
+pushd play-java-fileupload-example      && scripts/test-sbt && popd
+pushd play-java-forms-example           && scripts/test-sbt && popd
+pushd play-java-grpc-example            && scripts/test-sbt && popd
+pushd play-java-hello-world-tutorial    && scripts/test-sbt && popd
+pushd play-java-jpa-example             && scripts/test-sbt && popd
+pushd play-java-rest-api-example        && scripts/test-sbt && popd
+pushd play-java-starter-example         && scripts/test-sbt && popd
+pushd play-java-streaming-example       && scripts/test-sbt && popd
+pushd play-java-websocket-example       && scripts/test-sbt && popd
+pushd play-java-akka-cluster-example    && scripts/test-sbt && popd
+pushd play-scala-anorm-example          && scripts/test-sbt && popd
+pushd play-scala-chatroom-example       && scripts/test-sbt && popd
+pushd play-scala-compile-di-example     && scripts/test-sbt && popd
+pushd play-scala-fileupload-example     && scripts/test-sbt && popd
+pushd play-scala-forms-example          && scripts/test-sbt && popd
+pushd play-scala-grpc-example           && scripts/test-sbt && popd
+pushd play-scala-hello-world-tutorial   && scripts/test-sbt && popd
+pushd play-scala-isolated-slick-example && scripts/test-sbt && popd
+pushd play-scala-log4j2-example         && scripts/test-sbt && popd
+pushd play-scala-macwire-di-example     && scripts/test-sbt && popd
+pushd play-scala-rest-api-example       && scripts/test-sbt && popd
+# Uses libsodium
+# pushd play-scala-secure-session-example && scripts/test-sbt && popd
+pushd play-scala-slick-example          && scripts/test-sbt && popd
+pushd play-scala-starter-example        && scripts/test-sbt && popd
+pushd play-scala-streaming-example      && scripts/test-sbt && popd
+# uses vanilla sbt
+# pushd play-scala-tls-example            && scripts/test-sbt && popd
+pushd play-scala-websocket-example      && scripts/test-sbt && popd
