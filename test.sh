a7ae2b8e3ea4d0f1e79b8d1ee08fd277078e368f
Matthias Kurz <m.kurz@irregular.at>
2023-04-06 22:07:59 +0200

    Turn on Scala 3 on CI

--- test.sh
+++ test.sh
@@ -9,7 +9,9 @@ pushd play-java-dagger2-example         && scripts/test-sbt && popd
 pushd play-java-ebean-example           && scripts/test-sbt && popd
 pushd play-java-fileupload-example      && scripts/test-sbt && popd
 pushd play-java-forms-example           && scripts/test-sbt && popd
-pushd play-java-grpc-example            && scripts/test-sbt && popd
+if [ "$MATRIX_SCALA" != "3.x" ]; then
+  pushd play-java-grpc-example            && scripts/test-sbt && popd
+fi
 pushd play-java-hello-world-tutorial    && scripts/test-sbt && popd
 pushd play-java-jpa-example             && scripts/test-sbt && popd
 pushd play-java-rest-api-example        && scripts/test-sbt && popd
@@ -22,15 +24,21 @@ pushd play-scala-chatroom-example       && scripts/test-sbt && popd
 pushd play-scala-compile-di-example     && scripts/test-sbt && popd
 pushd play-scala-fileupload-example     && scripts/test-sbt && popd
 pushd play-scala-forms-example          && scripts/test-sbt && popd
-pushd play-scala-grpc-example           && scripts/test-sbt && popd
+if [ "$MATRIX_SCALA" != "3.x" ]; then
+  pushd play-scala-grpc-example           && scripts/test-sbt && popd
+fi
 pushd play-scala-hello-world-tutorial   && scripts/test-sbt && popd
-pushd play-scala-isolated-slick-example && scripts/test-sbt && popd
+if [ "$MATRIX_SCALA" != "3.x" ]; then
+  pushd play-scala-isolated-slick-example && scripts/test-sbt && popd
+fi
 pushd play-scala-log4j2-example         && scripts/test-sbt && popd
 pushd play-scala-macwire-di-example     && scripts/test-sbt && popd
 pushd play-scala-rest-api-example       && scripts/test-sbt && popd
 # Uses libsodium
 pushd play-scala-secure-session-example && scripts/test-sbt && popd
-pushd play-scala-slick-example          && scripts/test-sbt && popd
+if [ "$MATRIX_SCALA" != "3.x" ]; then
+  pushd play-scala-slick-example          && scripts/test-sbt && popd
+fi
 pushd play-scala-starter-example        && scripts/test-sbt && popd
 pushd play-scala-streaming-example      && scripts/test-sbt && popd
 # uses vanilla sbt
