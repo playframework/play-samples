b28c5f12fa082380421a67b657d082b9e1f8be08
Sergey Morgunov <hostage@me.com>
2023-12-01 14:33:38 +0300

    Gradle support preparation
    (cherry picked from commit e884039a85292b9f9f4f8958687030d9dd18fba9)

---
+++ test-gradle.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+if [ -z "$MATRIX_SCALA" ]; then
+    echo "Error: the environment variable MATRIX_SCALA is not set"
+    exit 1
+fi
+# pushd play-java-chatroom-example        && scripts/test-gradle && popd
+# pushd play-java-compile-di-example      && scripts/test-gradle && popd
+# pushd play-java-dagger2-example         && scripts/test-gradle && popd
+# pushd play-java-ebean-example           && scripts/test-gradle && popd
+# pushd play-java-fileupload-example      && scripts/test-gradle && popd
+# pushd play-java-forms-example           && scripts/test-gradle && popd
+#if [ "$MATRIX_SCALA" != "3.x" ]; then
+#  pushd play-java-grpc-example            && scripts/test-gradle && popd
+#fi
+# pushd play-java-hello-world-tutorial    && scripts/test-gradle && popd
+# pushd play-java-jpa-example             && scripts/test-gradle && popd
+# pushd play-java-rest-api-example        && scripts/test-gradle && popd
+# pushd play-java-starter-example         && scripts/test-gradle && popd
+# pushd play-java-streaming-example       && scripts/test-gradle && popd
+# pushd play-java-websocket-example       && scripts/test-gradle && popd
+# pushd play-java-pekko-cluster-example    && scripts/test-gradle && popd
+#pushd play-scala-anorm-example          && scripts/test-gradle && popd
+# pushd play-scala-chatroom-example       && scripts/test-gradle && popd
+# pushd play-scala-compile-di-example     && scripts/test-gradle && popd
+# pushd play-scala-fileupload-example     && scripts/test-gradle && popd
+# pushd play-scala-forms-example          && scripts/test-gradle && popd
+#if [ "$MATRIX_SCALA" != "3.x" ]; then
+#  pushd play-scala-grpc-example           && scripts/test-gradle && popd
+#fi
+# pushd play-scala-hello-world-tutorial   && scripts/test-gradle && popd
+# if [ "$MATRIX_SCALA" != "3.x" ]; then
+#   pushd play-scala-isolated-slick-example && scripts/test-gradle && popd
+# fi
+# pushd play-scala-log4j2-example         && scripts/test-gradle && popd
+# pushd play-scala-macwire-di-example     && scripts/test-gradle && popd
+# pushd play-scala-rest-api-example       && scripts/test-gradle && popd
+# Uses libsodium
+# pushd play-scala-secure-session-example && scripts/test-gradle && popd
+# if [ "$MATRIX_SCALA" != "3.x" ]; then
+#   pushd play-scala-slick-example          && scripts/test-gradle && popd
+# fi
+# pushd play-scala-starter-example        && scripts/test-gradle && popd
+# pushd play-scala-streaming-example      && scripts/test-gradle && popd
+# uses vanilla sbt
+# pushd play-scala-tls-example            && scripts/test-gradle && popd
+# pushd play-scala-websocket-example      && scripts/test-gradle && popd
