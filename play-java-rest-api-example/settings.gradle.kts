d012d9c4bbab7a91ec815d6c55486e4d6aac5ab9
Sergey Morgunov <hostage@me.com>
2023-12-01 13:45:51 +0300

    Gradle support for `play-java-rest-api-example`

---
+++ play-java-rest-api-example/settings.gradle.kts
@@ -0,0 +1,12 @@
+rootProject.name = "play-java-rest-api-example"
+
+// TODO: Remove after release Play Gradle Plugin
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+        }
+        mavenLocal()
+    }
+}
