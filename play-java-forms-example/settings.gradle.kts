5acfc09801e26a5d8c3c5b41cf07514c58aa7841
Sergey Morgunov <hostage@me.com>
2023-12-07 18:21:18 +0300

    Gradle support for `play-java-forms-example`

---
+++ play-java-forms-example/settings.gradle.kts
@@ -0,0 +1,12 @@
+rootProject.name = "play-java-forms-example"
+
+// TODO: Remove after release Play Gradle Plugin
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+        }
+        mavenLocal()
+    }
+}
