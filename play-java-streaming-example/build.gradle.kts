9dacf0d2c63d99baf9840b1206524b2b036d6f4c
Sergey Morgunov <hostage@me.com>
2023-12-11 16:16:33 +0300

    Gradle support for `play-java-streaming-example`

---
+++ play-java-streaming-example/build.gradle.kts
@@ -0,0 +1,39 @@
+import play.gradle.plugin.PlayPlugin
+
+plugins {
+    alias(libs.plugins.twirl)
+    alias(libs.plugins.play)
+}
+
+val scalaVersion = System.getProperty("scala.version", PlayPlugin.DEFAULT_SCALA_VERSION).trimEnd { !it.isDigit() }
+
+dependencies {
+    implementation(platform("org.playframework:play-bom_$scalaVersion:${libs.versions.play.get()}"))
+
+    implementation("org.playframework:play-pekko-http-server_$scalaVersion")
+    implementation("org.playframework:play-guice_$scalaVersion")
+    implementation("org.playframework:play-java-forms_$scalaVersion")
+    implementation("org.playframework:play-logback_$scalaVersion")
+    implementation("org.playframework:play-filters-helpers_$scalaVersion")
+
+    testImplementation(libs.junit)
+    testImplementation("org.playframework:play-test_$scalaVersion")
+}
+
+sourceSets {
+  main {
+    twirl {
+      templateImports.add("play.mvc.Http.{ RequestHeader => JRequestHeader }")
+      templateImports.add("views.html.helper.CSPNonce")
+    }
+  }
+}
+
+// TODO: Remove after release Play Gradle Plugin
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+    mavenLocal()
+}
