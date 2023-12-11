52473a955a647e502b15b51721188701edc2db63
Sergey Morgunov <hostage@me.com>
2023-12-11 16:16:33 +0300

    Gradle support for `play-java-streaming-example`

--- test-gradle.sh
+++ test-gradle.sh
@@ -14,7 +14,7 @@ pushd play-java-forms-example           && scripts/test-gradle && popd
 # pushd play-java-jpa-example             && scripts/test-gradle && popd
 pushd play-java-rest-api-example        && scripts/test-gradle && popd
 # pushd play-java-starter-example         && scripts/test-gradle && popd
-# pushd play-java-streaming-example       && scripts/test-gradle && popd
+pushd play-java-streaming-example       && scripts/test-gradle && popd
 pushd play-java-websocket-example       && scripts/test-gradle && popd
 # pushd play-java-pekko-cluster-example    && scripts/test-gradle && popd
 #pushd play-scala-anorm-example          && scripts/test-gradle && popd
