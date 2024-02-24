90c8b17f530f76923112fe87d8a1361fa8bc53e6
Matthias Kurz <m.kurz@irregular.at>
2024-02-25 00:32:57 +0100

    Fix tests

--- play-scala-slick-example/samples/computer-database/test/ApplicationSpec.scala
+++ play-scala-slick-example/samples/computer-database/test/ApplicationSpec.scala
@@ -21,53 +21,60 @@ class ApplicationSpec extends PlaySpecification {
     }
 
     "redirect to the computer list on /" in new WithApplication {
-      val result = applicationController.index(FakeRequest())
+      override def running() = {
+        val result = applicationController.index(FakeRequest())
 
-      status(result) must equalTo(SEE_OTHER)
-      redirectLocation(result) must beSome.which(_ == "/computers")
+        status(result) must equalTo(SEE_OTHER)
+        redirectLocation(result) must beSome[String].which(_ == "/computers")
+      }
     }
 
     "list computers on the the first page" in new WithApplication {
-      val result = applicationController.list(0, 2, "")(FakeRequest())
+      override def running() = {
+        val result = applicationController.list(0, 2, "")(FakeRequest())
 
-      status(result) must equalTo(OK)
-      contentAsString(result) must contain("574 computers found")
+        status(result) must equalTo(OK)
+        contentAsString(result) must contain("574 computers found")
+      }
     }
 
     "filter computer by name" in new WithApplication {
-      val result = applicationController.list(0, 2, "Apple")(FakeRequest())
+      override def running() = {
+        val result = applicationController.list(0, 2, "Apple")(FakeRequest())
 
-      status(result) must equalTo(OK)
-      contentAsString(result) must contain("13 computers found")
-
+        status(result) must equalTo(OK)
+        contentAsString(result) must contain("13 computers found")
+      }
     }
 
     "create new computer" in new WithApplication {
-      val badResult = applicationController.save(FakeRequest())
+      override def running() = {
+        val badResult = applicationController.save(FakeRequest())
 
-      status(badResult) must equalTo(BAD_REQUEST)
+        status(badResult) must equalTo(BAD_REQUEST)
 
-      val badDateFormat = applicationController.save(
-        FakeRequest().withFormUrlEncodedBody("name" -> "FooBar", "introduced" -> "badbadbad", "company" -> "1")
-      )
+        val badDateFormat = applicationController.save(
+          FakeRequest().withFormUrlEncodedBody("name" -> "FooBar", "introduced" -> "badbadbad", "company" -> "1")
+        )
 
-      status(badDateFormat) must equalTo(BAD_REQUEST)
-      contentAsString(badDateFormat) must contain("""<option value="1" selected="selected">Apple Inc.</option>""")
-      contentAsString(badDateFormat) must contain("""<input type="text" id="introduced" name="introduced" value="badbadbad" />""")
-      contentAsString(badDateFormat) must contain("""<input type="text" id="name" name="name" value="FooBar" />""")
+        status(badDateFormat) must equalTo(BAD_REQUEST)
+        contentAsString(badDateFormat) must contain("""<option value="1" selected="selected">Apple Inc.</option>""")
+        contentAsString(badDateFormat) must contain("""<input type="text" id="introduced" name="introduced" value="badbadbad" />""")
+        contentAsString(badDateFormat) must contain("""<input type="text" id="name" name="name" value="FooBar" />""")
 
-      val result = applicationController.save(
-        FakeRequest().withFormUrlEncodedBody("name" -> "FooBar", "introduced" -> "2011-12-24", "company" -> "1")
-      )
+        val result = applicationController.save(
+          FakeRequest().withFormUrlEncodedBody("name" -> "FooBar", "introduced" -> "2011-12-24", "company" -> "1")
+        )
 
-      status(result) must equalTo(SEE_OTHER)
-      redirectLocation(result) must beSome.which(_ == "/computers")
-      flash(result).get("success") must beSome.which(_ == "Computer FooBar has been created")
+        status(result) must equalTo(SEE_OTHER)
+        redirectLocation(result) must beSome[String].which(_ == "/computers")
+        flash(result).get("success") must beSome[String].which(_ == "Computer FooBar has been created")
 
-      val list = applicationController.list(0, 2, "FooBar")(FakeRequest())
+        val list = applicationController.list(0, 2, "FooBar")(FakeRequest())
 
-      status(list) must equalTo(OK)
-      contentAsString(list) must contain("One computer found")
+        status(list) must equalTo(OK)
+        contentAsString(list) must contain("One computer found")
+      }
     }
   }
 
