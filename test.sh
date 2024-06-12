4cf101e9f05181fecf5b32864736be0b7d05434e
Sergey Morgunov <hostage@me.com>
2024-06-12 15:15:57 +0300

    Gradle support for `play-scala-compile-di-example`

--- test.sh
+++ test.sh
@@ -75,10 +75,10 @@ else
   buildSample play-java-pekko-cluster-example
   buildSample play-scala-rest-api-example
   buildSample play-scala-chatroom-example
+  buildSample play-scala-compile-di-example
   if [[ $build_tool == "sbt" ]]; then
 #    buildSample play-scala-anorm-example
     buildSample play-java-grpc-example
-    buildSample play-scala-compile-di-example
     buildSample play-scala-fileupload-example
     buildSample play-scala-forms-example
     buildSample play-scala-grpc-example
