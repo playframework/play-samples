b1bee28646ae0665a0cc182d75b800fadd2ec482
Matthias Kurz <m.kurz@irregular.at>
2024-06-20 13:44:05 +0200

    Use WebJarsUtil in the scala websockets example
    (cherry picked from commit c5803745418a3f5a981df684b8b89e3a145801db)

--- play-scala-websocket-example/app/controllers/HomeController.scala
+++ play-scala-websocket-example/app/controllers/HomeController.scala
@@ -8,6 +8,7 @@ import akka.actor.typed.{ ActorRef, Scheduler }
 import akka.actor.typed.scaladsl.AskPattern._
 import akka.stream.scaladsl._
 import akka.util.Timeout
+import org.webjars.play.WebJarsUtil
 import play.api.Logger
 import play.api.libs.json._
 import play.api.mvc._
@@ -19,7 +20,7 @@ import scala.concurrent.{ ExecutionContext, Future }
  * This class creates the actions and the websocket needed.
  */
 @Singleton
-class HomeController @Inject()(userParentActor: ActorRef[UserParentActor.Create],
+class HomeController @Inject()(userParentActor: ActorRef[UserParentActor.Create], webJarsUtil: org.webjars.play.WebJarsUtil,
                                cc: ControllerComponents)
                               (implicit ec: ExecutionContext, scheduler: Scheduler)
   extends AbstractController(cc) with SameOriginCheck {
@@ -28,7 +29,7 @@ class HomeController @Inject()(userParentActor: ActorRef[UserParentActor.Create]
 
   // Home page that renders template
   def index = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(views.html.index(webJarsUtil))
   }
 
   /**
