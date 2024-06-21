a885f5146665fd85abbc12d0a191358d5aa910ce
Sergey Morgunov <hostage@me.com>
2024-06-21 16:46:11 +0300

    New folder structure [Java]

--- sources.sh
+++ sources.sh
@@ -1,64 +1,69 @@
 #!/usr/bin/env bash
 
-pushd play-java-dagger2-example
-zip -r -q ../play-java-dagger2-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-dagger2-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+# Java Examples
+pushd java
+pushd chatroom
+zip -r -q ../chatroom-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../chatroom-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
-pushd play-java-chatroom-example
-zip -r -q ../play-java-chatroom-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-chatroom-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+pushd compile-di
+zip -r -q ../compile-di-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../compile-di-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
-pushd play-java-compile-di-example
-zip -r -q ../play-java-compile-di-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-compile-di-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+pushd dagger2
+zip -r -q ../dagger2-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../dagger2-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
-pushd play-java-forms-example
-zip -r -q ../play-java-forms-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-forms-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+pushd ebean
+zip -r -q ../ebean-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../ebean-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
-pushd play-java-ebean-example
-zip -r -q ../play-java-ebean-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-ebean-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+pushd fileupload
+zip -r -q ../fileupload-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../fileupload-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
-pushd play-java-fileupload-example
-zip -r -q ../play-java-fileupload-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-fileupload-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+pushd forms
+zip -r -q ../forms-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../forms-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
-pushd play-java-jpa-example
-zip -r -q ../play-java-jpa-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-jpa-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+pushd grpc
+zip -r -q ../grpc-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../grpc-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
-pushd play-java-grpc-example
-zip -r -q ../play-java-grpc-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-grpc-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+pushd hello-world
+zip -r -q ../hello-world-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../hello-world-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
-pushd play-java-hello-world-tutorial
-zip -r -q ../play-java-hello-world-sbt-tutorial.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-hello-world-gradle-tutorial.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+pushd jpa
+zip -r -q ../jpa-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../jpa-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
-pushd play-java-streaming-example
-zip -r -q ../play-java-streaming-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-streaming-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+pushd pekko-cluster
+zip -r -q ../pekko-cluster-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../pekko-cluster-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
-pushd play-java-rest-api-example
-zip -r -q ../play-java-rest-api-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-rest-api-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+pushd rest-api
+zip -r -q ../rest-api-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../rest-api-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
-pushd play-java-starter-example
-zip -r -q ../play-java-starter-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-starter-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+pushd starter
+zip -r -q ../starter-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../starter-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
+pushd streaming
+zip -r -q ../streaming-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../streaming-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+popd
+pushd websocket
+zip -r -q ../websocket-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
+zip -r -q ../websocket-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
+popd
+popd
+
+# Scala Examples
 pushd play-scala-anorm-example
 zip -r -q ../play-scala-anorm-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
 zip -r -q ../play-scala-anorm-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
-popd
-pushd play-java-websocket-example
-zip -r -q ../play-java-websocket-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-websocket-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
-popd
-pushd play-java-pekko-cluster-example
-zip -r -q ../play-java-pekko-cluster-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
-zip -r -q ../play-java-pekko-cluster-gradle-example.zip . -x "project/*" -x "scripts/*" -x "*.sbt"
 popd
 pushd play-scala-fileupload-example
 zip -r -q ../play-scala-fileupload-sbt-example.zip . -x "gradle/*" -x "scripts/*" -x "*gradle*"
