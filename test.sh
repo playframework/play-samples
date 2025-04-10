5aa1a9cecfe7f36bd182f9b06f3f58836e8423f1
Sergey Morgunov <hostage@me.com>
2025-04-10 15:54:33 +0300

    [3.0.x] Test the minimal supported JDK 11

--- test.sh
+++ test.sh
@@ -60,35 +60,37 @@ if [ -n "$sample" ]; then
   buildSample $sample
 else
   # sample not set, therefore run all samples
-  buildSample play-java-chatroom-example
-  buildSample play-java-compile-di-example
-  buildSample play-java-dagger2-example
-  buildSample play-java-ebean-example
-  buildSample play-java-fileupload-example
-  buildSample play-java-forms-example
   buildSample play-java-hello-world-tutorial
-  buildSample play-java-jpa-example
-  buildSample play-java-rest-api-example
-  buildSample play-java-starter-example
-  buildSample play-java-streaming-example
-  buildSample play-java-websocket-example
-  buildSample play-java-pekko-cluster-example
-  buildSample play-scala-rest-api-example
-  buildSample play-scala-anorm-example
-  buildSample play-java-grpc-example
-  buildSample play-scala-chatroom-example
-  buildSample play-scala-compile-di-example
-  buildSample play-scala-fileupload-example
-  buildSample play-scala-forms-example
-  buildSample play-scala-grpc-example
-  buildSample play-scala-hello-world-tutorial
-  buildSample play-scala-isolated-slick-example
-  buildSample play-scala-log4j2-example
-  buildSample play-scala-macwire-di-example
-  buildSample play-scala-secure-session-example
-  buildSample play-scala-slick-example
-  buildSample play-scala-starter-example
-  buildSample play-scala-streaming-example
-  buildSample play-scala-tls-example
-  buildSample play-scala-websocket-example
+  if [[ $build_tool == "sbt" ]]; then
+    buildSample play-java-chatroom-example
+    buildSample play-java-compile-di-example
+    buildSample play-java-dagger2-example
+    buildSample play-java-ebean-example
+    buildSample play-java-fileupload-example
+    buildSample play-java-forms-example
+    buildSample play-java-jpa-example
+    buildSample play-java-rest-api-example
+    buildSample play-java-starter-example
+    buildSample play-java-streaming-example
+    buildSample play-java-websocket-example
+    buildSample play-java-pekko-cluster-example
+    buildSample play-scala-rest-api-example
+    buildSample play-scala-anorm-example
+    buildSample play-java-grpc-example
+    buildSample play-scala-chatroom-example
+    buildSample play-scala-compile-di-example
+    buildSample play-scala-fileupload-example
+    buildSample play-scala-forms-example
+    buildSample play-scala-grpc-example
+    buildSample play-scala-hello-world-tutorial
+    buildSample play-scala-isolated-slick-example
+    buildSample play-scala-log4j2-example
+    buildSample play-scala-macwire-di-example
+    buildSample play-scala-secure-session-example
+    buildSample play-scala-slick-example
+    buildSample play-scala-starter-example
+    buildSample play-scala-streaming-example
+    buildSample play-scala-tls-example
+    buildSample play-scala-websocket-example
+  fi
 fi
